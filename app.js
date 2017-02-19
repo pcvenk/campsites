fd20e506e0d82fd14a87862b71ae8cf7a7e6d206
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-02-19 18:23:22 +0100

    Added to routes, ejs as a templating engine, rendered ejs templates

--- app.js
+++ app.js
@@ -6,7 +6,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-   res.send('Landing Page');
+   res.render('home');
+});
+
+app.get('/campsites', function(req, res){
+   var campgrounds = [
+      {name: 'Zlatorog Bohinj', image:'http://www.photosforclass.com/download/14435096036'},
+      {name: 'Camping Sobed', image:'http://www.photosforclass.com/download/4835814837'},
+      {name: 'Camping Bled', image:'http://www.photosforclass.com/download/1342367857'},
+      {name: 'Spik', image:'http://www.photosforclass.com/download/15989950903'},
+      {name: 'Zelena Laguna', image:'http://www.photosforclass.com/download/1430198323'}
+   ];
+
+   res.render('campsites', {
+      campgrounds: campgrounds
+   });
 });
 
 app.listen(3000, function(){
