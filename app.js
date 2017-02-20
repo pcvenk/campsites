f6f67af388cae24f5594ae6bc50580315845ccdd
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-02-20 18:31:39 +0100

    Added the new campsite route, ejs template displaying a form for creating new campsites

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app.post('/campsites', function(req, res){
    res.send('You hit the post route!');
 });
 
+app.get('/campsites/new', function(req, res){
+   res.render('newcampsite');
+});
+
 app.listen(3000, function(){
    console.log('Server started on port 3000');
 });
