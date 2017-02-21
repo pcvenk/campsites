73e52e9afba72cb0a6e2a204482320a3c013dc65
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-02-21 18:28:47 +0100

    Done: post campistes route stores to DB

--- app.js
+++ app.js
@@ -13,35 +13,6 @@ var campgroundSchema = new mongoose.Schema({
 
 var Campground = mongoose.model('Campground', campgroundSchema);
 
-// var newCampground = {
-//     name: 'Zlatorog Bled',
-//     image: 'http://www.photosforclass.com/download/14435096036'
-// };
-//
-//
-// Campground.create(newCampground, function(err, campground){
-//         if(err){
-//             console.log(err);
-//         } else {
-//             console.log('New Campsite Added');
-//             console.log(campground);
-//         }
-//     }
-// );
-
-
-
-// var campgrounds = [
-//     {name: 'Zlatorog Bohinj', image: 'http://www.photosforclass.com/download/14435096036'},
-//     {name: 'Camping Sobed', image: 'http://www.photosforclass.com/download/4835814837'},
-//     {name: 'Camping Bled', image: 'http://www.photosforclass.com/download/1342367857'},
-//     {name: 'Spik', image: 'http://www.photosforclass.com/download/15989950903'},
-//     {name: 'Zelena Laguna', image: 'http://www.photosforclass.com/download/1430198323'},
-//     {name: 'Zlatorog Bohinj', image: 'http://www.photosforclass.com/download/14435096036'},
-//     {name: 'Camping Sobed', image: 'http://www.photosforclass.com/download/4835814837'},
-//     {name: 'Camping Bled', image: 'http://www.photosforclass.com/download/1342367857'}
-// ];
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -77,10 +48,14 @@ app.post('/campsites', function (req, res) {
         name: name,
         image: image
     };
-    //adding new site to the array
-    campgrounds.push(newCampsite);
-
-    res.redirect('/campsites');
+    //creating a new site and saving it to DB
+    Campground.create(newCampsite, function(err, newlyCreated){
+       if(err){
+           console.log(err);
+       } else {
+           res.redirect('/campsites');
+       }
+    });
 });
 
 app.get('/campsites/new', function (req, res) {
