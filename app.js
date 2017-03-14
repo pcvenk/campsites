d68b195fd822f689404831ceed75c30f8554a191
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-03-14 16:23:14 +0100

    Added main.css in the public folder

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 //setting up static assets
-app.use(express.static('public'));
+app.use(express.static(__dirname +'/public'));
 
 app.get('/', function (req, res) {
     res.render('home');
