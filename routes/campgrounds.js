147fed3e326c7e984e6b0b00062f56401ba03dae
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-03-27 19:03:42 +0200

    Author data upon creating new campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -21,11 +21,16 @@ router.post('/', isLoggedIn, function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
 
     var newCampsite = {
         name: name,
         image: image,
-        description: description
+        description: description,
+        author: author
     };
     //creating a new site and saving it to DB
     Campground.create(newCampsite, function(err, newlyCreated){
