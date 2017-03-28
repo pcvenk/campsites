b3f0e918ad406ea5e746fc95876c9bcdbabe22b7
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-03-28 15:35:18 +0200

    User Authorization Middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -126,4 +126,24 @@ function isLoggedIn(req, res, next){
 
 }
 
+function campgroundOwnershipCheck(req, res, next){
+    //is the user logged in
+    if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground){
+            if(err){
+                res.redirect('back');
+            } else {
+                //does the user own the campground
+                if(foundCampground.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect('back');
+                }
+            }
+        });
+    } else {
+          res.redirect('back');
+    }
+}
+
 module.exports = router;
