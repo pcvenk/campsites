33e0b2f19941b41e5bbfecb3cec03491c0526c9c
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-04-03 15:19:07 +0200

    Fixed the rendering of the register page

--- routes/auth.js
+++ routes/auth.js
@@ -12,9 +12,8 @@ router.post('/register', function(req, res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if(err){
-            console.log(err);
             req.flash('error', err);
-            return res.render('/register');
+            return res.render('register');
         }
         passport.authenticate('local')(req, res, function() {
             req.flash('success', 'Welcome, '+user.username);
